3b5580ce934690ff77be367d3d201577cc229dfd
Jenn Yacoubian <jyacoubian@gmail.com>
2022-08-25 20:08:39 -0600

    Pass shelf and unshelf unit tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,20 @@
+function shelfBook(book, shelf) {
+  if(shelf.length < 3) {
+    return shelf.unshift(book);
+  }
+}
+
+function unshelfBook(book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (book === shelf[i].title) {
+      return shelf.splice(i, 1);
+    }
+  }
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
+  shelfBook,
+  unshelfBook,
   // listTitles,
   // searchShelf
 };
