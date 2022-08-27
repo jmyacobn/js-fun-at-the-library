9eb1f3163be78dc9e272b00fadb59bf0bf719f1d
Jenn Yacoubian <jyacoubian@gmail.com>
2022-08-26 18:11:35 -0600

    Pass create library unit tests

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,17 @@
-
+function createLibrary (name, type) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+  return library;
+}
 
 module.exports = {
-  // createLibrary,
+  createLibrary,
   // addBook,
   // checkoutBook
 };
