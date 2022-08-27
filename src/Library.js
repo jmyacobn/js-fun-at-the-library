02ad1f8f797b1ad3b8ec9f21c7577cbe7c0d277c
Jenn Yacoubian <jyacoubian@gmail.com>
2022-08-26 19:00:52 -0600

    Complete library tests

--- src/Library.js
+++ src/Library.js
@@ -1,4 +1,4 @@
-function createLibrary (name, type) {
+function createLibrary(name, type) {
   var library = {
     name: name,
     shelves: {
@@ -10,7 +10,7 @@ function createLibrary (name, type) {
   return library;
 }
 
-function addBook (library, book) {
+function addBook(library, book) {
   if (book.genre === "fantasy") {
     library.shelves.fantasy.push(book);
   } else if (book.genre === "nonFiction") {
@@ -20,9 +20,18 @@ function addBook (library, book) {
   }
 }
 
+function checkoutBook(library, book, type) {
+  for (var i = 0; i < library.shelves[type].length; i++) {
+    if (book === library.shelves[type][i].title) {
+      library.shelves[type].splice(i,1);
+      return `You have now checked out ${book} from the ${library.name}`
+    }
+  }
+      return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+}
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
